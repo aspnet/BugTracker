d6e104fd191e41514d9f83cdb8deb3cef066a69e
Praburaj <praburaj.t@microsoft.com>
2014-08-07 16:39:10 -0700

    Adding Kestrel package to BugTracker
    Also fixed a URL issue. EncodeUriComponent renders the URI incorrect as it escapes / to %2F.
    WebListener unescapes the / somehow making even an invalid URL to work. But Kestrel does not escape %2F resulting in 404.
    Adding some instructions in readme as well.

--- src/BugTracker/Startup.cs
+++ src/BugTracker/Startup.cs
@@ -1,53 +1,49 @@
-using Microsoft.AspNet;
 using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Http;
 using Microsoft.Framework.DependencyInjection;
 using Microsoft.AspNet.Diagnostics;
-using Microsoft.AspNet.Mvc;
 using Microsoft.AspNet.Routing;
-using Microsoft.AspNet.SignalR;
 
-    public class Startup
+public class Startup
+{
+    public void Configure(IBuilder app)
     {
-        public void Configure(IBuilder app)
+        /* Error page middleware displays a nice formatted HTML page for any unhandled exceptions in the request pipeline.
+        * Note: ErrorPageOptions.ShowAll to be used only at development time. Not recommended for production.
+        */
+        app.UseErrorPage(ErrorPageOptions.ShowAll);
+
+        app.UseServices(services =>
         {
-            /* Error page middleware displays a nice formatted HTML page for any unhandled exceptions in the request pipeline.
-            * Note: ErrorPageOptions.ShowAll to be used only at development time. Not recommended for production.
-            */
-            app.UseErrorPage(ErrorPageOptions.ShowAll);
+            //Add all MVC related services to IoC.
+            services.AddMvc();
 
-            app.UseServices(services =>
-            {
-                //Add all MVC related services to IoC.
-                services.AddMvc();
+            //Add all SignalR related services to IoC.
+            services.AddSignalR();
+        });
 
-                //Add all SignalR related services to IoC.
-                services.AddSignalR();
-            });
+        //Configure SignalR
+        app.UseSignalR();
 
-            //Configure SignalR
-            app.UseSignalR();
+        //Serves static files in the application.
+        app.UseFileServer();
 
-            //Serves static files in the application.
-            app.UseFileServer();
+        //Configure WebFx
+        app.UseMvc(routes =>
+        {
+            routes.MapRoute(
+                null,
+                "{controller}/{action}",
+                new { controller = "Home", action = "Index" });
 
-            //Configure WebFx
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    null,
-                    "{controller}/{action}",
-                    new { controller = "Home", action = "Index" });
+            routes.MapRoute(
+                null,
+                "api/{controller}/{action}",
+                new { controller = "Home", action = "Index" });
 
-                routes.MapRoute(
-                    null,
-                    "api/{controller}/{action}",
-                    new { controller = "Home", action = "Index" });
-
-                routes.MapRoute(
-                    null,
-                    "api/{controller}",
-                    new { controller = "Home" });
-            });
-        }
+            routes.MapRoute(
+                null,
+                "api/{controller}",
+                new { controller = "Home" });
+        });
     }
+}
